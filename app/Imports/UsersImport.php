28f7921103a1db891b21a72f07490005755dee17
Monir Ahmed <monir.ahmed0993@gmail.com>
2020-09-23 18:31:03 +0600

    User Import File Updated

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -5,8 +5,9 @@ namespace App\Imports;
 use App\User;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class UsersImport implements ToModel
+class UsersImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -16,10 +17,10 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            'name'     => $row[0],
-            'email'    => $row[1], 
-            'password' => Hash::make($row[2]),
-            'user_type' => $row[3]
+            'name'     => $row['name'],
+            'email'    => $row['email'], 
+            'password' => Hash::make($row['password']),
+            'user_type' => $row['user_type']
         ]);
     }
 }
